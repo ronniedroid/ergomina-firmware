add503c732d83aefd5bd65b5d82fd5e69d70acf9
ronniedroid <ronnie.nissan@proton.me>
2024-05-13 13:00:45 +0300

    changed layout

--- kb.py
+++ kb.py
@@ -5,7 +5,7 @@ from kmk.scanners import DiodeOrientation
 
 
 class KMKKeyboard(_KMKKeyboard):
-    row_pins = (board.GP1, board.GP2, board.GP4, board.GP6, board.GP5)
+    row_pins = (board.GP2, board.GP4, board.GP6, board.GP5)
     col_pins = (
         board.GP9,
         board.GP7,
@@ -26,9 +26,8 @@ class KMKKeyboard(_KMKKeyboard):
     # flake8: noqa
     # fmt: off
     coord_mapping = [
-         0,  1,  2,  3,  4,   5,  6,  7,  8,  9, 10, 11,
-        12, 13, 14, 15, 16,  17, 18, 19, 20, 21, 22, 23,
-        24, 25, 26, 27, 28,  29, 30, 31, 32, 33, 34, 35,
-        36, 37, 38, 39, 40,  41, 42, 43, 44, 45, 46, 47,
-                50, 51, 52,  53, 54, 55, 56
+         0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11,
+        12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+        24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+                38, 39, 40, 41, 42, 43, 44
      ]
--- main.py
+++ main.py
@@ -6,37 +6,61 @@ from kmk.scanners import DiodeOrientation
 from kmk.modules.layers import Layers
 from kmk.extensions.stringy_keymaps import StringyKeymaps
 from kmk.extensions.media_keys import MediaKeys
-from kmk.modules.modtap import ModTap
+from kmk.modules.holdtap import HoldTap
 
-Iris = KMKKeyboard()
+Ergomina = KMKKeyboard()
 
-Iris.modules.append(Layers())
-Iris.extensions.append(StringyKeymaps())
-Iris.extensions.append(MediaKeys())
-Iris.modules.append(ModTap())
+Ergomina.modules.append(Layers())
+Ergomina.extensions.append(StringyKeymaps())
+Ergomina.extensions.append(MediaKeys())
+Ergomina.modules.append(HoldTap())
+
+Ergomina.debug_enabled = True
 
 # Layers
 QWERTY = KC.DF(0)
 COLEMAK = KC.DF(1)
-GAME = KC.DF(5)
 LOWER = KC.MO(2)
 RAISE = KC.MO(3)
-ADJUST = KC.MO(4)
 
 # Keys
 xx = "NO"
 __ = "TRNS"
-LGUI = KC.MT(KC.ENT, KC.LGUI)  # Enter when tapped, left GUI when held
-LSFT = KC.MT(KC.Z, KC.LSFT)  # a when tapped, left shift when held
-RSFT = KC.MT(KC.SLSH, KC.RSFT)
-LLSFT = KC.MT(KC.AT, KC.LSFT)
-LRSFT = KC.MT(KC.PIPE, KC.RSFT)
+LGUI = KC.HT(KC.ENT, KC.LM(4, KC.LGUI), prefer_hold=True, tap_interrupted=False, tap_time=250)  # Enter when tapped, left GUI when held
+LSFT = KC.HT(KC.Z, KC.LSFT)  # a when tapped, left shift when held
+RSFT = KC.HT(KC.SLSH, KC.RSFT)
+LLSFT = KC.HT(KC.AT, KC.LSFT)
+LRSFT = KC.HT(KC.PIPE, KC.RSFT)
+# Gnome macros
+MWSR = KC.PGDN  # Move to right workspace 
+MWSL = KC.PGUP  # Move to left workspace 
+MWSF = KC.HOME # Move to the first workspace 
+MWSE = KC.END # Move to the last workspace 
+KWIN = KC.W  # Kill window
+SAPP = KC.TAB # Switch apps
+SWIN = KC.GRV # Switch windows
+HWIN = KC.H # Hide window
+TERM = KC.T # Open terminal
+OVIEW = KC.X # Open overview
+MWIN = KC.UP # Toggle maximization
+RWIN = KC.DOWN # Restore window size
+CWIN = KC.C # Move window to center (tiling assistant)
+TWL = KC.LEFT # Tile window to the left
+TWR = KC.RIGHT # Tile window to the right
+TWTL = KC.P7 # Tile window top left
+TWTR = KC.P9 # Tile window top left
+TWBL = KC.P1 # Tile window top left
+TWBR = KC.P3 # Tile window top left
+APPS = KC.A # Show all apps
+NOTIF = KC.V # Show the notification list
+QMENU = KC.S # Show the quick settings menu
+RUN = KC.R # Show the run command prompt
+SLANG = KC.SPC # Switch input language
 
 # fmt: off
-Iris.keymap = [
+Ergomina.keymap = [
     # layer 0, Qwerty
     [
-            xx,      xx,     xx,     xx,    xx,    xx,    xx,     xx,      xx,      xx,      xx,      xx,
           'TAB',    'Q',    'W',    'E',   'R',   'T',   'Y',    'U',     'I',     'O',     'P',   'ESC',
          'BSPC',    'A',    'S',    'D',   'F',   'G',   'H',    'J',     'K',     'L',  'SCLN',  'QUOT',
              xx,   LSFT,    'X',    'C',   'V',   'B',   'N',    'M',  'COMM',   'DOT',    RSFT,      xx,
@@ -44,7 +68,6 @@ Iris.keymap = [
     ],
     # layer 1, Colemak
     [
-             xx,    xx,     xx,     xx,    xx,    xx,    xx,     xx,      xx,      xx,      xx,      xx,
           'TAB',   'Q',    'W',    'F',   'P',   'B',   'J',    'L',     'U',     'Y',  'SCLN',   'ESC',
          'BSPC',   'A',    'R',    'S',   'T',   'G',   'M',    'N',     'E',     'I',     'O',  'QUOT',
              xx,  LSFT,    'X',    'C',   'D',   'V',   'K',    'H',  'COMM',   'DOT',    RSFT,      xx,
@@ -52,42 +75,29 @@ Iris.keymap = [
     ],
         # layer 2, LOWER
     [
-            xx,     xx,    xx,     xx,    xx,      xx,      xx,     xx,      xx,      xx,      xx,      xx,
          'TAB',   'N0',  'N7',   'N8',  'N9',  'PLUS',  'HASH',  'DLR',  'PERC',  'CIRC',   'AMPR', 'ASTR',
         'BSNC', 'EXLM',  'N4',   'N5',  'N6',  'MINS',  'LPRN', 'RPRN',  'LBRC',  'RBRC',   'BSLS',  'GRV',
            xx,   LLSFT,  'N1',   'N2',  'N3',   'EQL',  'LCBR', 'RCBR',  'LABK',  'RABK',   LRSFT,     xx,
-                           __,     __,     __,     xx,      __,    ADJUST,      __,
+                           __,     __,     'LGUI',     xx,      __,    __,      __,
     ],
 
 
         # layer 3, RAISE
     [
-          xx,     xx,     xx,       xx,    xx,    xx,      xx,      xx,      xx,       xx,      xx,    xx,
-       'INS',     xx,   'F1',     'F2',  'F3',  'F4',  'VOLD',  'BRID',  'BRIU',   'VOLU',  'PSCR',    xx,
-       'DEL',     xx,   'F5',     'F6',  'F7',  'F8',  'LEFT',  'DOWN',    'UP',  'RIGHT',  'MUTE',    xx,
-          xx, 'LSFT',   'F9',    'F10', 'F11', 'F12',  'HOME',  'PGDN',  'PGUP',    'END',  'RSFT',    xx,
-                          __,   ADJUST,    __,    xx,      __,      __,      __,
+       'INS',  QWERTY,   'F1',     'F2',  'F3',  'F4',  'VOLD',  'BRID',  'BRIU',   'VOLU',  'PSCR',    xx,
+       'DEL', COLEMAK,   'F5',     'F6',  'F7',  'F8',  'LEFT',  'DOWN',    'UP',  'RIGHT',  'MUTE',    xx,
+          xx,  'LSFT',   'F9',    'F10', 'F11', 'F12',  'HOME',  'PGDN',  'PGUP',    'END',  'RSFT',    xx,
+                          __,   __,    'LGUI',    xx,      __,      __,      __,
     ],
-        # layer 4, ADJUST
+        # layer 4, Gnome
     [
-       xx,      xx,   xx,       xx,       xx,    xx,  xx,  xx,  xx,      xx,  xx,    xx,
-       xx,  QWERTY,   xx,       xx,  'RESET',    xx,  xx,  xx,  xx,      xx,  xx,    xx,
-       xx,      xx,   xx,  'DEBUG',       xx,    GAME,  xx,  xx,  xx,   'RLD',  xx,    xx,
-       xx,      xx,   xx,  COLEMAK,       xx,    xx,  xx,  xx,  xx,      xx,  xx,    xx,
-                      __,       __,       __,    xx,  __,  __,  __,
+     SAPP,    KWIN,     SWIN,       xx,    RUN,  TERM,   TWTL,  TWBL,  TWBR,   TWTR, 'PSCR',     xx,
+     HWIN,    APPS,    QMENU,       xx,     xx,    xx,    TWL,  RWIN,  MWIN,    TWR,   CWIN,     xx,
+       xx,  'LSFT',    OVIEW,       xx,  NOTIF,    xx,   MWSF,  MWSL,  MWSR,   MWSE,     xx,     xx,
+                          __,       __,     __,    xx,  SLANG,    __,    __,
     ],
-    [
-        # layer 5, GAME
-            xx,      xx,     xx,     xx,    xx,    xx,    xx,     xx,      xx,      xx,      xx,      xx,
-          'ESC',    'Q',    'W',    'E',   'R',   'T',   'Y',    'U',     'I',     'O',     'P',   'ESC',
-         'ENT',    'A',    'S',    'D',   'F',   'G',   'H',    'J',     'K',     'L',  'SCLN',   ADJUST,
-             xx,    'Z',    'X',    'C',   'V',   'B',   'N',    'M',  'COMM',   'DOT',    RSFT,      xx,
-                        'LCTRL',  'LSFT',  'SPC',  xx, 'BSPC',  'LGUI',  'LALT',
-    ],
-
-    
 ]
 
 
 if __name__ == "__main__":
-    Iris.go()
+    Ergomina.go()
